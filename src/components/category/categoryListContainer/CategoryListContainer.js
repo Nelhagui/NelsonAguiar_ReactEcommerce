30daea4091c52e314a83232e7a2d716a02dcf8ea
Nelson <nelson.aguiar@reiatsu.com.ar>
2021-07-04 03:14:22 -0300

    Corrección cambio de categoria

--- src/components/category/categoryListContainer/CategoryListContainer.js
+++ src/components/category/categoryListContainer/CategoryListContainer.js
@@ -3,24 +3,18 @@ import { useState, useEffect } from 'react';
 import { ItemList } from '../../itemsContainer/itemList/ItemList';
 
 export const CategoryListContainer = ({match}) => {
-    let categoryID = match.params.categoryId
-    console.log('id select', categoryID)
     const [categories, setCategories] = useState([]);
     useEffect(() => {
-        console.log('ajax')
         fetch('https://mocki.io/v1/37f3a056-9a4f-4a2f-9f9b-17fb48ee962c')
-            .then((response) => response.json())
-            .then((res) => findCategory(res))
-    }, [])
-    console.log(categories);
-    //FUNCION TEMPORAL HASTA ARMAR API CON "/id"
-    const findCategory = (res) => 
-    {
-        console.log('ejecuto')
-        var results = res.filter(function (category) { return category.id_category === +categoryID; });
-        setCategories(results);
-    }
-    //fin
+        .then((response) => response.json())
+        .then((res) => findCategory(res))
+        const findCategory = (res) => 
+        {
+            let results = res.filter(function (category) { return category.id_category === +match.params.categoryId; });
+            setCategories(results);
+        }
+    }, [match])
+
 
     return (
             <div className="containerBooks">
--- src/components/itemsContainer/itemDetailContainer/ItemDetailContainer.js
+++ src/components/itemsContainer/itemDetailContainer/ItemDetailContainer.js
@@ -3,22 +3,23 @@ import { ItemDetail } from './itemDetail/ItemDetail';
 import { useState, useEffect } from 'react';
 
 export const ItemDetailContainer = ({match}) => {
-    let itemID = match.params.id
     const [detailMatch, setDetailMatch] = useState([]);
     useEffect(() => {
         fetch('https://mocki.io/v1/37f3a056-9a4f-4a2f-9f9b-17fb48ee962c')
             .then((response) => response.json())
             .then((res) => findMatch(res))
-    }, [])
 
-    //FUNCION TEMPORAL HASTA ARMAR API CON "/id"
-    const findMatch = (res) => 
-    {
-        var results = res.filter(function (detail) { return detail.id === +itemID; });
-        var firstObj = (results.length > 0) ? results[0] : null;
-        setDetailMatch(firstObj);
-    }
-    //fin
+        //FUNCION TEMPORAL HASTA ARMAR API CON "/id"
+        const findMatch = (res) => 
+        {
+            var results = res.filter(function (detail) { return +detail.id === +match.params.id; });
+            var firstObj = (results.length > 0) ? results[0] : null;
+            setDetailMatch(firstObj);
+        }
+        //fin
+
+    }, [match])
+
 
     return (
         <div>
